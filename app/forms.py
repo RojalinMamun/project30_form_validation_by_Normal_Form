2fca40055f555ba8ea754bca19cf333cb002e34c
Rojalin Das <dasrosalin670@gmail.com>
2023-04-26 11:42:01 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -11,6 +11,19 @@ def check_for_len(value):
 
 
 class StudentForm(forms.Form):
-    name=forms.CharField(max_length=100)
+    name=forms.CharField(max_length=100,validators=[check_for_a,check_for_len])
     age=forms.IntegerField()
-    email=forms.EmailField(validators=[check_for_a])
+    email=forms.EmailField()
+    re_enter_email=forms.EmailField()
+
+    def clean(self):
+        e=self.cleaned_data['email']
+        r=self.cleaned_data['re_enter_email']
+        if e!=r:
+            raise forms.ValidationError('not matched')
+    
+
+    def clean_botcatcher(self):
+        bot=self.cleaned_data['botcatcher']
+        if len(bot)>0:
+            raise forms.ValidationError('bot')
