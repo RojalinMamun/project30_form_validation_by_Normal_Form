573852fa773815d05b3ddd3f690e442644ca5f1c
Rojalin Das <dasrosalin670@gmail.com>
2023-04-26 21:27:34 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core import validators
 
 def check_for_a(value):
     if value[0].lower()=='a':
@@ -11,10 +12,11 @@ def check_for_len(value):
 
 
 class StudentForm(forms.Form):
-    name=forms.CharField(max_length=100,validators=[check_for_a,check_for_len])
+    name=forms.CharField(max_length=100,validators=[check_for_a,validators.MaxLengthValidator(5)])
     age=forms.IntegerField()
     email=forms.EmailField()
     re_enter_email=forms.EmailField()
+    mobile=forms.CharField(max_length=10,min_length=10,validators=[validators.RegexValidator('[6-9]\d{9}')])
 
     def clean(self):
         e=self.cleaned_data['email']
